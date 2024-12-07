9dc003eeded8272216e9a9fc0b26dbbc067db7d5
vigneshhari <vichuhari100@gmail.com>
2024-12-08 01:46:27 +0300

    fix tiny bug

--- care/emr/api/viewsets/units.py
+++ care/emr/api/viewsets/units.py
@@ -1,12 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from care.emr.units.units_client import UnitsClient
+from care.emr.units.units_client import units_client
 
 
 class UnitsView(GenericViewSet):
     def list(self, request, *args, **kwargs):
-        client = UnitsClient()
-        client.create_cache()
-        results = client.search(request.query_params.get("search", ""))
+        results = units_client.search(request.query_params.get("search", ""))
         return Response({"count": len(results), "results": results})
