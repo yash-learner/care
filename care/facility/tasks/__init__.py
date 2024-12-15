b1d3ac0f4da9470c6efad0f8bb06bf238f70f4b5
Areeb Ahmed <areebahmed0709@gmail.com>
2024-12-15 22:32:27 +0530

    Remove periodic summarization tasks from tasks/__init__.py

--- care/facility/tasks/__init__.py
+++ care/facility/tasks/__init__.py
@@ -23,36 +23,6 @@ def setup_periodic_tasks(sender, **kwargs):
         delete_old_notifications.s(),
         name="delete_old_notifications",
     )
-    if settings.TASK_SUMMARIZE_TRIAGE:
-        sender.add_periodic_task(
-            crontab(hour="*/4", minute="59"),
-            summarize_triage.s(),
-            name="summarise_triage",
-        )
-    if settings.TASK_SUMMARIZE_TESTS:
-        sender.add_periodic_task(
-            crontab(hour="23", minute="59"),
-            summarize_tests.s(),
-            name="summarise_tests",
-        )
-    if settings.TASK_SUMMARIZE_FACILITY_CAPACITY:
-        sender.add_periodic_task(
-            crontab(minute="*/5"),
-            summarize_facility_capacity.s(),
-            name="summarise_facility_capacity",
-        )
-    if settings.TASK_SUMMARIZE_PATIENT:
-        sender.add_periodic_task(
-            crontab(hour="*/1", minute="59"),
-            summarize_patient.s(),
-            name="summarise_patient",
-        )
-    if settings.TASK_SUMMARIZE_DISTRICT_PATIENT:
-        sender.add_periodic_task(
-            crontab(hour="*/1", minute="59"),
-            summarize_district_patient.s(),
-            name="summarise_district_patient",
-        )
 
     sender.add_periodic_task(
         crontab(minute="*/30"),
