4eb5e68d2431b6aa61e3a9648ce1cc827df8fa7d
vigneshhari <vichuhari100@gmail.com>
2024-12-29 00:17:49 +0530

    Possibly fix issues with lazy loading

--- care/emr/models/__init__.py
+++ care/emr/models/__init__.py
@@ -5,3 +5,4 @@ from .questionnaire import *  # noqa F403
 from .valueset import *  # noqa F403
 from .scheduling import *  # noqa F403
 from .encounter import *  # noqa F403
+from .patient import * # noqa F403
