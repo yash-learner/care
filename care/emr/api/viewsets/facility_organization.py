e4892a4191a18c061b02e555f9699bb428af29c4
vigneshhari <vichuhari100@gmail.com>
2025-01-04 23:30:33 +0530

    Add my organization API in facility

--- care/emr/api/viewsets/facility_organization.py
+++ care/emr/api/viewsets/facility_organization.py
@@ -1,7 +1,9 @@
 from django.db.models import Q
 from django_filters import rest_framework as filters
+from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied, ValidationError
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
 from care.emr.api.viewsets.base import EMRModelViewSet
 from care.emr.models.organization import FacilityOrganization, FacilityOrganizationUser
@@ -132,6 +134,20 @@ class FacilityOrganizationViewSet(EMRModelViewSet):
             self.request.user,
             facility,
         )
+
+    @action(detail=False, methods=["GET"])
+    def mine(self, request, *args, **kwargs):
+        """
+        Get organizations that are directly attached to the given user
+        """
+        orgusers = FacilityOrganizationUser.objects.filter(
+            user=request.user, organization__facility=self.get_facility_obj()
+        ).select_related("organization")
+        data = [
+            self.get_read_pydantic_model().serialize(orguser.organization).to_json()
+            for orguser in orgusers
+        ]
+        return Response({"count": len(data), "results": data})
 
 
 class FacilityOrganizationUsersViewSet(EMRModelViewSet):
